9a87d2da58b80d4b7fcb13062e8e0e723b68cfc0
hussamk98 <hussamkhan98@gmail.com>
2020-04-12 01:56:41 +0500

    remove click listener on a white image

--- app.js
+++ app.js
@@ -79,6 +79,10 @@ document.addEventListener('DOMContentLoaded', () => {
       alert('You found a match')
       cards[optionOneId].setAttribute('src', 'images/white.png')
       cards[optionTwoId].setAttribute('src', 'images/white.png')
+
+      cards[optionOneId].removeEventListener('click', flipCard);
+      cards[optionTwoId].removeEventListener('click', flipCard);
+      
       cardsWon.push(cardsChosen)
     } else {
       cards[optionOneId].setAttribute('src', 'images/blank.png')
