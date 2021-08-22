eedc094b83b4ef8789cef9d1f82856b381635f78
Brandon Swenson <brandon@pcfrontier.com>
2021-08-22 01:34:24 -0400

    Limits the amount of cards that can be flipped to 2.

--- app.js
+++ app.js
@@ -103,12 +103,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
   //flip your card
   function flipCard() {
-    let cardId = this.getAttribute('data-id')
-    cardsChosen.push(cardArray[cardId].name)
-    cardsChosenId.push(cardId)
-    this.setAttribute('src', cardArray[cardId].img)
-    if (cardsChosen.length ===2) {
-      setTimeout(checkForMatch, 500)
+    if (cardsChosen.length < 2) {
+      let cardId = this.getAttribute('data-id')
+      cardsChosen.push(cardArray[cardId].name)
+      cardsChosenId.push(cardId)
+      this.setAttribute('src', cardArray[cardId].img)
+      if (cardsChosen.length ===2) {
+        setTimeout(checkForMatch, 500)
+      }
     }
   }
 
