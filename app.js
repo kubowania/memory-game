d8aac69626f45088d2ed572817406acf6d287d53
Kevin Bell <bellKevin@pm.me>
2022-12-14 08:57:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ document.addEventListener('DOMContentLoaded', () => {
     cardsChosenId = []
     resultDisplay.textContent = cardsWon.length
     if  (cardsWon.length === cardArray.length/2) {
-      resultDisplay.textContent = 'Congratulations! You found them all!'
+      resultDisplay.textContent = ' Congratulations! You found them all!'
     }
   }
 
